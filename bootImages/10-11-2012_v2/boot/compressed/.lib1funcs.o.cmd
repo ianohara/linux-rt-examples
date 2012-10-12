8ac389decc171b92235913d4f880848a37764bbe
ianohara <ianohara@f9728153-16c7-43d0-9843-c13daa3a4bf4>
2012-10-12 01:19:00 +0000

    Added attempt at much smaller image without using Sakomon config as a basis, but instead just using his CONFIG_CMDLINE.  It did not work. See README in 10-11-2012_v2
    git-svn-id: http://svn.modlabupenn.org/personal/iano/realtime_gumstix@311 f9728153-16c7-43d0-9843-c13daa3a4bf4

---
+++ bootImages/10-11-2012_v2/boot/compressed/.lib1funcs.o.cmd
@@ -0,0 +1,34 @@
+cmd_arch/arm/boot/compressed/lib1funcs.o := gcc -Wp,-MD,arch/arm/boot/compressed/.lib1funcs.o.d  -nostdinc -isystem /usr/lib/gcc/arm-linux-gnueabi/4.4.5/include -I/home/captain/linux-stable-rt/arch/arm/include -Iarch/arm/include/generated -Iinclude  -include /home/captain/linux-stable-rt/include/linux/kconfig.h -D__KERNEL__ -mlittle-endian -Iarch/arm/mach-omap2/include -Iarch/arm/plat-omap/include -D__ASSEMBLY__ -mabi=aapcs-linux -mno-thumb-interwork  -D__LINUX_ARM_ARCH__=6 -march=armv6k -mtune=arm1136j-s -include asm/unified.h -msoft-float    -Wa,-march=all     -c -o arch/arm/boot/compressed/lib1funcs.o arch/arm/boot/compressed/lib1funcs.S
+
+source_arch/arm/boot/compressed/lib1funcs.o := arch/arm/boot/compressed/lib1funcs.S
+
+deps_arch/arm/boot/compressed/lib1funcs.o := \
+    $(wildcard include/config/aeabi.h) \
+  /home/captain/linux-stable-rt/arch/arm/include/asm/unified.h \
+    $(wildcard include/config/arm/asm/unified.h) \
+    $(wildcard include/config/thumb2/kernel.h) \
+  include/linux/linkage.h \
+  include/linux/compiler.h \
+    $(wildcard include/config/sparse/rcu/pointer.h) \
+    $(wildcard include/config/trace/branch/profiling.h) \
+    $(wildcard include/config/profile/all/branches.h) \
+    $(wildcard include/config/enable/must/check.h) \
+    $(wildcard include/config/enable/warn/deprecated.h) \
+  /home/captain/linux-stable-rt/arch/arm/include/asm/linkage.h \
+  /home/captain/linux-stable-rt/arch/arm/include/asm/assembler.h \
+    $(wildcard include/config/cpu/feroceon.h) \
+    $(wildcard include/config/trace/irqflags.h) \
+    $(wildcard include/config/smp.h) \
+  /home/captain/linux-stable-rt/arch/arm/include/asm/ptrace.h \
+    $(wildcard include/config/cpu/endian/be8.h) \
+    $(wildcard include/config/arm/thumb.h) \
+  /home/captain/linux-stable-rt/arch/arm/include/asm/hwcap.h \
+  /home/captain/linux-stable-rt/arch/arm/include/asm/domain.h \
+    $(wildcard include/config/io/36.h) \
+    $(wildcard include/config/cpu/use/domains.h) \
+  /home/captain/linux-stable-rt/arch/arm/include/asm/unwind.h \
+    $(wildcard include/config/arm/unwind.h) \
+
+arch/arm/boot/compressed/lib1funcs.o: $(deps_arch/arm/boot/compressed/lib1funcs.o)
+
+$(deps_arch/arm/boot/compressed/lib1funcs.o):

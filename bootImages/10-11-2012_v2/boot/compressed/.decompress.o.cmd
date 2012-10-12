21067dff1782c97aaec628d45a0cc2636cafd251
ianohara <ian.ohara+rt-linux@gmail.com>
2012-10-12 01:19:00 +0000

    Added attempt at much smaller image without using Sakomon config as a basis, but instead just using his CONFIG_CMDLINE.  It did not work. See README in 10-11-2012_v2
    git-svn-id: http://svn.modlabupenn.org/personal/iano/realtime_gumstix@311 f9728153-16c7-43d0-9843-c13daa3a4bf4

---
+++ bootImages/10-11-2012_v2/boot/compressed/.decompress.o.cmd
@@ -0,0 +1,63 @@
+cmd_arch/arm/boot/compressed/decompress.o := gcc -Wp,-MD,arch/arm/boot/compressed/.decompress.o.d  -nostdinc -isystem /usr/lib/gcc/arm-linux-gnueabi/4.4.5/include -I/home/captain/linux-stable-rt/arch/arm/include -Iarch/arm/include/generated -Iinclude  -include /home/captain/linux-stable-rt/include/linux/kconfig.h -D__KERNEL__ -mlittle-endian -Iarch/arm/mach-omap2/include -Iarch/arm/plat-omap/include -Wall -Wundef -Wstrict-prototypes -Wno-trigraphs -fno-strict-aliasing -fno-common -Werror-implicit-function-declaration -Wno-format-security -fno-delete-null-pointer-checks -O2 -marm -fno-dwarf2-cfi-asm -fno-omit-frame-pointer -mapcs -mno-sched-prolog -mabi=aapcs-linux -mno-thumb-interwork -D__LINUX_ARM_ARCH__=6 -march=armv6k -mtune=arm1136j-s -msoft-float -Uarm -Wframe-larger-than=1024 -fno-stack-protector -fno-omit-frame-pointer -fno-optimize-sibling-calls -Wdeclaration-after-statement -Wno-pointer-sign -fno-strict-overflow -fconserve-stack -fpic -fno-builtin -Iarch/arm/boot/compressed    -D"KBUILD_STR(s)=\#s" -D"KBUILD_BASENAME=KBUILD_STR(decompress)"  -D"KBUILD_MODNAME=KBUILD_STR(decompress)" -c -o arch/arm/boot/compressed/.tmp_decompress.o arch/arm/boot/compressed/decompress.c
+
+source_arch/arm/boot/compressed/decompress.o := arch/arm/boot/compressed/decompress.c
+
+deps_arch/arm/boot/compressed/decompress.o := \
+    $(wildcard include/config/kernel/gzip.h) \
+    $(wildcard include/config/kernel/lzo.h) \
+    $(wildcard include/config/kernel/lzma.h) \
+  include/linux/compiler.h \
+    $(wildcard include/config/sparse/rcu/pointer.h) \
+    $(wildcard include/config/trace/branch/profiling.h) \
+    $(wildcard include/config/profile/all/branches.h) \
+    $(wildcard include/config/enable/must/check.h) \
+    $(wildcard include/config/enable/warn/deprecated.h) \
+  include/linux/compiler-gcc.h \
+    $(wildcard include/config/arch/supports/optimized/inlining.h) \
+    $(wildcard include/config/optimize/inlining.h) \
+  include/linux/compiler-gcc4.h \
+  include/linux/types.h \
+    $(wildcard include/config/uid16.h) \
+    $(wildcard include/config/lbdaf.h) \
+    $(wildcard include/config/arch/dma/addr/t/64bit.h) \
+    $(wildcard include/config/phys/addr/t/64bit.h) \
+    $(wildcard include/config/64bit.h) \
+  /home/captain/linux-stable-rt/arch/arm/include/asm/types.h \
+  include/asm-generic/int-ll64.h \
+  arch/arm/include/generated/asm/bitsperlong.h \
+  include/asm-generic/bitsperlong.h \
+  include/linux/posix_types.h \
+  include/linux/stddef.h \
+  /home/captain/linux-stable-rt/arch/arm/include/asm/posix_types.h \
+  include/linux/linkage.h \
+  /home/captain/linux-stable-rt/arch/arm/include/asm/linkage.h \
+  /home/captain/linux-stable-rt/arch/arm/include/asm/string.h \
+  arch/arm/boot/compressed/../../../../lib/decompress_inflate.c \
+  arch/arm/boot/compressed/../../../../lib/zlib_inflate/inftrees.c \
+  include/linux/zutil.h \
+  include/linux/zlib.h \
+  include/linux/zconf.h \
+  include/linux/string.h \
+    $(wildcard include/config/binary/printf.h) \
+  include/linux/kernel.h \
+    $(wildcard include/config/preempt/voluntary.h) \
+    $(wildcard include/config/debug/atomic/sleep.h) \
+    $(wildcard include/config/prove/locking.h) \
+    $(wildcard include/config/ring/buffer.h) \
+    $(wildcard include/config/tracing.h) \
+    $(wildcard include/config/numa.h) \
+    $(wildcard include/config/compaction.h) \
+    $(wildcard include/config/ftrace/mcount/record.h) \
+  arch/arm/boot/compressed/../../../../lib/zlib_inflate/inftrees.h \
+  arch/arm/boot/compressed/../../../../lib/zlib_inflate/inffast.c \
+    $(wildcard include/config/have/efficient/unaligned/access.h) \
+  arch/arm/boot/compressed/../../../../lib/zlib_inflate/inflate.h \
+  arch/arm/boot/compressed/../../../../lib/zlib_inflate/inffast.h \
+  arch/arm/boot/compressed/../../../../lib/zlib_inflate/inflate.c \
+  arch/arm/boot/compressed/../../../../lib/zlib_inflate/infutil.h \
+  arch/arm/boot/compressed/../../../../lib/zlib_inflate/inffixed.h \
+  include/linux/decompress/mm.h \
+
+arch/arm/boot/compressed/decompress.o: $(deps_arch/arm/boot/compressed/decompress.o)
+
+$(deps_arch/arm/boot/compressed/decompress.o):

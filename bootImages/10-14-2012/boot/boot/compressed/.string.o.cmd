8cf93fa36279a1076b8873d141b140a455115803
ianohara <ian.ohara+rt-linux@gmail.com>
2012-10-15 22:13:33 +0000

    Added the boot image for the 10-14-2012 iteration.  Also added note that the modules did not build, even though the image did.
    git-svn-id: http://svn.modlabupenn.org/personal/iano/realtime_gumstix@325 f9728153-16c7-43d0-9843-c13daa3a4bf4

---
+++ bootImages/10-14-2012/boot/boot/compressed/.string.o.cmd
@@ -0,0 +1,36 @@
+cmd_arch/arm/boot/compressed/string.o := gcc -Wp,-MD,arch/arm/boot/compressed/.string.o.d  -nostdinc -isystem /usr/lib/gcc/arm-linux-gnueabi/4.4.5/include -I/home/captain/linux-stable-rt/arch/arm/include -Iarch/arm/include/generated -Iinclude  -include /home/captain/linux-stable-rt/include/linux/kconfig.h -D__KERNEL__ -mlittle-endian -Iarch/arm/mach-omap2/include -Iarch/arm/plat-omap/include -Wall -Wundef -Wstrict-prototypes -Wno-trigraphs -fno-strict-aliasing -fno-common -Werror-implicit-function-declaration -Wno-format-security -fno-delete-null-pointer-checks -O2 -marm -fno-dwarf2-cfi-asm -mabi=aapcs-linux -mno-thumb-interwork -funwind-tables -D__LINUX_ARM_ARCH__=6 -march=armv6k -mtune=arm1136j-s -msoft-float -Uarm -Wframe-larger-than=1024 -fno-stack-protector -fomit-frame-pointer -g -Wdeclaration-after-statement -Wno-pointer-sign -fno-strict-overflow -fconserve-stack -fpic -fno-builtin -Iarch/arm/boot/compressed -Os    -D"KBUILD_STR(s)=\#s" -D"KBUILD_BASENAME=KBUILD_STR(string)"  -D"KBUILD_MODNAME=KBUILD_STR(string)" -c -o arch/arm/boot/compressed/.tmp_string.o arch/arm/boot/compressed/string.c
+
+source_arch/arm/boot/compressed/string.o := arch/arm/boot/compressed/string.c
+
+deps_arch/arm/boot/compressed/string.o := \
+  include/linux/string.h \
+    $(wildcard include/config/binary/printf.h) \
+  include/linux/compiler.h \
+    $(wildcard include/config/sparse/rcu/pointer.h) \
+    $(wildcard include/config/trace/branch/profiling.h) \
+    $(wildcard include/config/profile/all/branches.h) \
+    $(wildcard include/config/enable/must/check.h) \
+    $(wildcard include/config/enable/warn/deprecated.h) \
+  include/linux/compiler-gcc.h \
+    $(wildcard include/config/arch/supports/optimized/inlining.h) \
+    $(wildcard include/config/optimize/inlining.h) \
+  include/linux/compiler-gcc4.h \
+  include/linux/types.h \
+    $(wildcard include/config/uid16.h) \
+    $(wildcard include/config/lbdaf.h) \
+    $(wildcard include/config/arch/dma/addr/t/64bit.h) \
+    $(wildcard include/config/phys/addr/t/64bit.h) \
+    $(wildcard include/config/64bit.h) \
+  /home/captain/linux-stable-rt/arch/arm/include/asm/types.h \
+  include/asm-generic/int-ll64.h \
+  arch/arm/include/generated/asm/bitsperlong.h \
+  include/asm-generic/bitsperlong.h \
+  include/linux/posix_types.h \
+  include/linux/stddef.h \
+  /home/captain/linux-stable-rt/arch/arm/include/asm/posix_types.h \
+  /usr/lib/gcc/arm-linux-gnueabi/4.4.5/include/stdarg.h \
+  /home/captain/linux-stable-rt/arch/arm/include/asm/string.h \
+
+arch/arm/boot/compressed/string.o: $(deps_arch/arm/boot/compressed/string.o)
+
+$(deps_arch/arm/boot/compressed/string.o):

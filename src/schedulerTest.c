f98f2f5c30a92dda3c1a7973d6d491f368267f00
uriahb <uriahb@f9728153-16c7-43d0-9843-c13daa3a4bf4>
2012-11-10 03:10:47 +0000

    Started breaking out all of the scheduling status/state prints into functions since they will be called after I set all of the settings they report on.
    git-svn-id: http://svn.modlabupenn.org/personal/iano/realtime_gumstix@423 f9728153-16c7-43d0-9843-c13daa3a4bf4

--- src/schedulerTest.c
+++ src/schedulerTest.c
@@ -1,9 +1,13 @@
 /*
- * Test out all of the scheduling system calls.  
+ * Examples of using all of the scheduling system calls.
  *  A nice summary of all of the scheduling system calls can
  *  be found at:
  *      http://www.makelinux.net/books/ulk3/understandlk-CHP-7-SECT-6#understandlk-CHP-7-SECT-6
  *
+ * System Calls Exemplified Here:
+ *   sched_get_priority_max()
+ *   sched_get_priority_min()
+ *   sched_getparam()
  * Ian O'Hara
  * ianohara at gmail dot com
  * October 25, 2012
@@ -30,6 +34,22 @@ static const int have_sched_prio = 1;
 #else
 static const int have_sched_prio = 0;
 #endif /* _POSIX_PRIORITY_SCHEDULING */
+
+/* For mapping sched_policy integer -> corresponding string name.  The #defines
+ * can be found by looking in /usr/include/sched.h and following #includes
+ * until you find the SCHED_* definitions.
+ *
+ * These are for OS X 10.6.8 and I found the defines
+ * in: /usr/include/pthread_impl.h
+ */
+const static int SCHED_POLICY_MAX = 4;
+char *sched_policies[] = {
+    "0_NOT_A_POLICY",
+    "SCHED_OTHER",
+    "SCHED_RR",
+    "3_NOT_A_POLICY",
+    "SCHED_FIFO"
+};
 
 int
 main(int argc, char *argv[])
@@ -69,26 +89,53 @@ main(int argc, char *argv[])
            min_prio_RR,
            min_prio_OTHER);
 
+    print_process_prio(0);
+    print_sched_type(0);
+    print_sched_priority(0);
+}
+
+
+void
+print_process_prio(pid_t pid)
+{
     /* Get the proirity of this process. This is the NON-REALTIME priority.
      *   http://linux.die.net/man/2/getpriority
      */
     /* This can return -1 as a legitimate value, so first clear errno
      * and make sure to check it after the call.
      */
-    pid_t this_pid = 0; /* 0 refers to the current thread/process in most (all?) system calls */
     errno = 0;
-    int this_prio = getpriority(PRIO_PROCESS, this_pid);
+    int this_prio = getpriority(PRIO_PROCESS, pid);
     if ((this_prio == -1) && (errno)) {
         perror("Syscall getpriority failed");
     } else {
         printf("Process priority is: %i\n", this_prio);
     }
+}
 
+void
+print_sched_type(pid_t pid)
+{
+    int policy = sched_getscheduler(pid);
+    if (policy < 0) {
+        perror("Syscall sched_getscheduler() had problems");
+        return;
+    }
+    if (policy > SCHED_POLICY_MAX) {
+        printf("Syscall sched_getscheduler() returned a policy number greater than allowed!\n");
+        return;
+    }
+    printf("The current scheduling policy is: %s\n", sched_policies[policy]);
+}
+
+void
+print_sched_priority(pid_t pid)
+{
     /* Get and inspect the scheduling parameters.  This can contain realtime relevant
      * information if this process is operating with SCHED_FIFO
      */
     struct sched_param param;
-    if (sched_getparam(this_pid, &param) < 0) {
+    if (sched_getparam(pid, &param) < 0) {
         perror("Syscall sched_getparam barfed");
     } else {
         printf("The sched_param schedule priority is: %i\n", param.sched_priority);
